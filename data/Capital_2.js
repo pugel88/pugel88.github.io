13610e9253665738e431947768f861c4d8d87fc5
pugel88 <47930781+pugel88@users.noreply.github.com>
2019-02-23 15:17:38 -0800

    Add files via upload

---
+++ data/Capital_2.js
@@ -0,0 +1,8 @@
+var json_Capital_2 = {
+"type": "FeatureCollection",
+"name": "Capital_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "scalerank": 0.0, "natscale": 600.0, "labelrank": 1.0, "featurecla": "Admin-0 capital", "name": "Washington, D.C.", "namepar": null, "namealt": "Washington D.C.", "diffascii": 0.0, "nameascii": "Washington, D.C.", "adm0cap": 1.0, "capalt": null, "capin": null, "worldcity": 1.0, "megacity": 1.0, "sov0name": "United States", "sov_a3": "USA", "adm0name": "United States of America", "adm0_a3": "USA", "adm1name": "District of Columbia", "iso_a2": "US", "note": null, "latitude": 38.899549376499998, "longitude": -77.009418580800002, "changed": 0.0, "namediff": 0.0, "diffnote": null, "pop_max": 4338000.0, "pop_min": 552433.0, "pop_other": 2175991.0, "rank_max": 12.0, "rank_min": 11.0, "geonameid": 4140963.0, "meganame": "Washington, D.C.", "ls_name": "Washington, D.C.", "ls_match": 1.0, "checkme": 5.0, "min_zoom": 2.1 }, "geometry": { "type": "Point", "coordinates": [ -77.011364439437159, 38.901495235086998 ] } }
+]
+}
